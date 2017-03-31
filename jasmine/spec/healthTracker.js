cf80ea7c7693cfa9fd5d0e2e5c20de833d403b0f
Daniele <daniele.rb6@gmail.com>
2017-03-31 10:34:56 +0200

    refactor: Delete enclosing function

--- jasmine/spec/healthTracker.js
+++ jasmine/spec/healthTracker.js
@@ -4,29 +4,27 @@
  * all of the tests that will be run against our application.
  */
 
-$(function() {
-	/* The first test helps ensure that when creating instances without
-	 * a value set for any specific attribute, a default one (e.g. an empty
-	 * screen) is used instead.
-	 * This ensures that the application will interact with our models
-	 * without any unexpected behavior.
-	 */
-	 describe('New food items tests', function() {
-	 	it('can be created with default attributes for their values', function() {
-	 		var foodItem = new FoodItem();
-	 		expect(foodItem.get('brandName')).toBe('');
-	 		expect(foodItem.get('itemName')).toBe('');
-	 		expect(foodItem.get('itemCalories')).toBe('');
-	 		expect(foodItem.get('itemQuantity')).toBe(1);
-	 	});
+/* The first test helps ensure that when creating instances without
+ * a value set for any specific attribute, a default one (e.g. an empty
+ * screen) is used instead.
+ * This ensures that the application will interact with our models
+ * without any unexpected behavior.
+ */
+describe('New food items tests', function() {
+ 	it('can be created with default attributes for their values', function() {
+ 		var foodItem = new FoodItem();
+ 		expect(foodItem.get('brandName')).toBe('');
+ 		expect(foodItem.get('itemName')).toBe('');
+ 		expect(foodItem.get('itemCalories')).toBe('');
+ 		expect(foodItem.get('itemQuantity')).toBe(1);
+ 	});
 
-	 	it('can be created with overridden attributes for their values', function() {
-	 		var foodItem = new FoodItem();
-	 		foodItem.set({
-	 			itemQuantity: 5
-	 		});
-	 		expect(foodItem.get('brandName')).toBe('');
-	 		expect(foodItem.get('itemQuantity')).toBe(5);
-	 	});
-	 });
+ 	it('can be created with overridden attributes for their values', function() {
+ 		var foodItem = new FoodItem();
+ 		foodItem.set({
+ 			itemQuantity: 5
+ 		});
+ 		expect(foodItem.get('brandName')).toBe('');
+ 		expect(foodItem.get('itemQuantity')).toBe(5);
+ 	});
 });

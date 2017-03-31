df23d0ad4dfb80c93ef0a89d0c0a98711bc30036
Daniele <daniele.rb6@gmail.com>
2017-03-31 18:01:19 +0200

    feat: Test an AJAX request

--- jasmine/spec/healthTracker.js
+++ jasmine/spec/healthTracker.js
@@ -9,10 +9,21 @@
  * screen) is used instead.
  * This ensures that the application will interact with our models
  * without any unexpected behavior.
+ *
+ * Code taken from Addy Osmani's book 'Developing Backbone.js Applications' and
+ * adapted for this application. The actual section of the book where this code
+ * resides is [Unit Testing, Jasmine - Suites, Specs and Spies](
+ * https://addyosmani.com/backbone-fundamentals/#suites-specs-spies).
  */
 describe('New food items tests', function() {
+	// Place the instance of FoodItem in the same Functional Scope
+	var foodItem = new FoodItem();
+
+	it('should be correctly defined', function() {
+		expect(foodItem).toBeDefined();
+	});
+
  	it('can be created with default attributes for their values', function() {
- 		var foodItem = new FoodItem();
  		expect(foodItem.get('brandName')).toBe('');
  		expect(foodItem.get('itemName')).toBe('');
  		expect(foodItem.get('itemCalories')).toBe('');
@@ -20,7 +31,6 @@ describe('New food items tests', function() {
  	});
 
  	it('can be created with overridden attributes for their values', function() {
- 		var foodItem = new FoodItem();
  		foodItem.set({
  			itemQuantity: 5
  		});
@@ -28,3 +38,44 @@ describe('New food items tests', function() {
  		expect(foodItem.get('itemQuantity')).toBe(5);
  	});
 });
+
+/* The second test helps test an AJAX request and verify that it was both calling
+ * the correct URL and executed a callback.
+ *
+ * Code taken from Addy Osmani's book 'Developing Backbone.js Applications' and
+ * adapted for this application. The actual section of the book where this code
+ * resides is [Unit Testing, Jasmine - Suites, Specs and Spies](
+ * https://addyosmani.com/backbone-fundamentals/#suites-specs-spies).
+ */
+describe('An AJAX request', function() {
+	// Fake an AJAX request and verify that the request was both calling the
+	// correct URL and executed a callback where one was provided
+	it('the callback should be executed on success', function() {
+		// Call a passed function when a spy has been called
+ 		spyOn($, 'ajax').and.callFake(function(options) {
+ 			options.success();
+ 		});;
+
+ 		// Create a new spy
+ 		var callback = jasmine.createSpy();
+
+ 		// Execute the spy callback if the request is successful
+ 		getFoodItem(callback);
+
+ 		// Verify that the url of the most recent call matches
+ 		// the expected FoodItem
+ 		expect($.ajax.calls.mostRecent().args[0]['url']).toEqual('/fooditems/');
+
+ 		expect(callback).toHaveBeenCalled();
+ 	});
+});
+
+function getFoodItem(callback) {
+	var queryUrl = '/fooditems/';
+	$.ajax({
+		url: queryUrl,
+		type: 'get',
+		dataType: 'json',
+		success: callback
+	});
+}

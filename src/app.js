27eba24a50aa1ab48fa9636906c85d7982c09678
Daniele <daniele.rb6@gmail.com>
2017-04-04 13:44:40 +0200

    refactor: Change foos items and clean code

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+/* The skeleton of this code was taken from a test guided exercise that I did
+ * to learn Backbone. The code is almost identical to that of the exercise and
+ * has just been adapted to the purpose of this app.
+ */
 var totalCalories = 0;
 
 var FoodItem = Backbone.Model.extend({
@@ -17,36 +21,36 @@ var foodItem = new FoodItem();
 
 var foodItemsList = new FoodItemsList([
 	{
-		brandName: 'Coca Cola',
-		itemName: 'Coca',
-		itemCalories: '200',
+		brandName: 'Pizza Hut',
+		itemName: 'Cheese Only Pizza',
+		itemCalories: '589',
 		itemQuantity: 1
 	},
 	{
-		brandName: 'McDonald',
-		itemName: 'McBurger',
-		itemCalories: '1000',
+		brandName: 'McDonald\'s',
+		itemName: 'Cheeseburger',
+		itemCalories: '300',
 		itemQuantity: 1
 	},
 	{
-		brandName: 'Nestle',
-		itemName: 'Chocolate',
-		itemCalories: '2500',
+		brandName: 'Starbucks',
+		itemName: 'Hot Chocolate with Whole Milk',
+		itemCalories: '165',
 		itemQuantity: 1
 	}
 ]);
 
 foodItemsList.add({
-	brandName: 'McPizza',
-	itemName: 'pizza Nutella Gigante',
-	itemCalories: '5000',
+	brandName: 'Dunkin\'Donuts',
+	itemName: 'Blueberry Cake Donut',
+	itemCalories: '340',
 	itemQuantity: 1
 });
 
 console.log(foodItemsList.toJSON());
 console.log(foodItemsList.models[0].get('itemCalories'));
 
-
+// Calculate the total calories
 for(var i=0; i < foodItemsList.length; i++) {
 	totalCalories += parseInt(foodItemsList.models[i].get('itemCalories'));
 }
@@ -55,10 +59,12 @@ console.log(totalCalories);
 
 var FoodItemsView = Backbone.View.extend({
 	render: function() {
+		// Render the food items
 		var source = $('#food-template').html();
 		var template = Handlebars.compile(source);
 		var rendered = template({foodItemsList: this.collection.toJSON()});
 		this.$el.append(rendered);
+		// Render the total calories
 		var source2 = $('#calories-sum').html();
 		var template2 = Handlebars.compile(source2);
 		var context = { calories: totalCalories };

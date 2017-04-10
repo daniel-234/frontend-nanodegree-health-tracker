e7f3259b0fbb352bce3aa0f183a91d2e2d444e2c
Daniele <daniele.rb6@gmail.com>
2017-04-10 10:20:19 +0200

    refactor: Move Backbone code to `app.js`

--- src/app.js
+++ src/app.js
@@ -1,87 +1,38 @@
-// var data;
-
-var FoodItem = Backbone.Model.extend({
-	defaults: {
-		brandName: '',
-		itemName: '',
-		itemCalories: '',
-		itemQuantity: 1
-	}
-});
-
-var foodItem = new FoodItem();
-console.log(foodItem.get('itemQuantity'));
-
-var FoodItemList = Backbone.Collection.extend({
-	model: FoodItem
-});
-
-var foodListCollection = new FoodItemList();
-
-function processData(response) {
-	// Add a Model to the Collection for each of the 20 query result
-	for(var i = 0; i < 20; i++) {
-		foodListCollection.add({
-			brandName: response.hits[i].fields.brand_name,
-			itemName: response.hits[i].fields.item_name,
-			itemCalories: response.hits[i].fields.nf_calories,
-			itemQuantity: response.hits[i].fields.nf_serving_size_qty
+var templateString = '<h3>I really like <%= who %>.</h3><h3>Lets\'s see how many calories it got.' +
+	' Uhm, it has <%= cal %>!</h3>';
+var AppView = Backbone.View.extend({
+	// The element associated with the View, where content will be rendered
+	el: '#container',
+	// Add a JS Undescore template, that can seem an unnecessary solution in this case,
+	// but will help to create dynamic rendering when things get more complicated.
+	// Template has the placeholder 'who' that will be substituted later in our code.
+	template: _.template(templateString),
+	// Call initialize when this view is instantiated
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		// URL to query the Nutritionix API, copied from the 'Get Started' section of their website.
+		var queryUrl = 'https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:2&' +
+			'fields=item_name,brand_name,item_id,nf_calories&appId=497ef47e&appKey=790e824a496fcc65e9fa3132a5d2d8fb';
+		var itemName,
+			itemCalories;
+		// Store the value of `this` inside the View, to use it later inside the callback function.
+		var that = this;
+		$.ajax({
+			url: queryUrl,
+			type: 'get',
+			dataType: 'json',
+			success: function(result) {
+				itemName = result.hits[0].fields.item_name;
+				itemCalories = result.hits[0].fields.nf_calories;
+				// Use jQuery to get the 'el' element in this View and set its HTML content
+				// to the template string, assigning a value to the placeholders 'who' and 'cal.
+				that.$el.html(that.template({who: itemName, cal: itemCalories}));
+			}
 		});
 	}
-
-	// Log each itemName attribute
-	foodListCollection.forEach(function(model) {
-		console.log(model.get('itemName'));
-	});
-
-	console.log(foodListCollection.toJSON());
-
-
-	console.log(response.hits[0].fields);
-	console.log('Brand name: ' + response.hits[0].fields.brand_name);
-	console.log('Item name: ' + response.hits[0].fields.item_name);
-	console.log('Calories: ' + response.hits[0].fields.nf_calories);
-	console.log('Serving size quantity: ' + response.hits[0].fields.nf_serving_size_qty);
-
-	var foodItem = 'Brand name: ' + response.hits[0].fields.brand_name + ' ; ' +
-		'Item name: ' + response.hits[0].fields.item_name + ' ; ' +
-		'Calories: ' + response.hits[0].fields.nf_calories + ' ; ' +
-		'Serving quantity: ' + response.hits[0].fields.nf_serving_size_qty;
-	$('#items').append(foodItem);
-}
-
-retrieveValues(processData);
-
-// Code taken from file CollectionViewSpec.js found in the
-// Backbone course hosted in 'teaching-materials.org'.
-var FoodItemView = Backbone.View.extend({
-	render: function() {
-		console.log(foodListCollection.toJSON());
-		var source = $('#food-template').html();
-		var template = Handlebars.compile(source);
-		var rendered = template({foodListCollection: this.collection.toJSON()});
-		this.$el.append(rendered);
-		return this;
-	}
 });
 
-var foodItemView = new FoodItemView({
-	collection: foodListCollection
-});
-
-// console.log(foodListCollection);
-
-function retrieveValues(callback) {
-	var queryUrl = 'https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:20&' +
-		'fields=item_name,brand_name,item_id,nf_calories&appId=497ef47e&appKey=790e824a496fcc65e9fa3132a5d2d8fb';
-	$.ajax({
-		url: queryUrl,
-		type: 'get',
-		dataType: 'json',
-		success: callback
-	});
-}
-
-$(document).ready(function() {
-	$('body').append(foodItemView.render().$el);
-});
+// Initialize the View.
+var appView = new AppView();

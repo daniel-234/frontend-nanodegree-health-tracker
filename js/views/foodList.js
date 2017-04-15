029238bb8ba3aeb91b0c5f51ba41706208f50c05
Daniele <daniele.rb6@gmail.com>
2017-04-15 15:13:10 +0200

    refactor: Add an array of models to Collection
    Modified the code to add an array of models to the Collection. Each Model should have its unique URL.

--- js/views/foodList.js
+++ js/views/foodList.js
@@ -1,16 +1,22 @@
 var app = app || {};
-var url = 'https://api.nutritionix.com/v1_1/search/' + 'mcdonalds' + '?results=0:2&' +
-		'fields=item_name,brand_name,item_id,nf_calories&appId=497ef47e&appKey=790e824a496fcc65e9fa3132a5d2d8fb';
+var url1 = 'https://api.nutritionix.com/v1_1/search/' + 'mcdonalds' + '?results=0:2&' +
+	'fields=item_name,brand_name,item_id,nf_calories&appId=497ef47e&appKey=790e824a496fcc65e9fa3132a5d2d8fb';
+var url2 = 'https://api.nutritionix.com/v1_1/search/' + 'mcdonalds' + '?results=3:5&' +
+	'fields=item_name,brand_name,item_id,nf_calories&appId=497ef47e&appKey=790e824a496fcc65e9fa3132a5d2d8fb';
 
 app.FoodListView = Backbone.View.extend({
 	el: '#foods',
 
 	initialize: function() {
 		this.collection = new app.FoodList();
-		this.food = new app.Food({url: url});
-		console.log(this.food.url);
+		this.food1 = new app.Food({url: url1});
+		this.food2 = new app.Food({url: url2});
+		console.log(this.food1.url);
 		// console.log(this.food.query);
-		this.collection.add(this.food);
+		this.collection.add([
+			this.food1,
+			this.food2
+		]);
 		this.render();
 	},
 	render: function() {
